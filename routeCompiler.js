ab6403ea77d6c4a0981f002f207348f9b452270e
camhux <camzig@gmail.com>
2015-09-08 10:15:30 -0700

    Initial commit: handles params, but not optionals or partials

---
+++ routeCompiler.js
@@ -0,0 +1,27 @@
+function routeCompiler(string) {
+  var rawString = "^\\/";
+  var pathSegs;
+  var parsedSeg;
+  var colon;
+  var i;
+
+  if (string.charAt(0) === '/') string = string.slice(1);
+
+  pathSegs = string.split('/');
+
+  for (i = 0; i < pathSegs.length; ++i) {
+    parsedSeg = "";
+    colon = pathSegs[i].indexOf(':');
+    if (colon === 0) {
+      parsedSeg += "(.*)"
+    } else {
+      parsedSeg += pathSegs[i];
+    }
+    parsedSeg += (i === pathSegs.length - 1) ? "" : "\\/"
+    rawString += parsedSeg;
+  }
+
+  rawString += "$";
+
+  return new RegExp(rawString);
+}
